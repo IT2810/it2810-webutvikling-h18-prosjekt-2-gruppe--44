e2f0bcb7e23ba2e453c0a19abc01fe6026ff51e8
Kenneth Wannebo <kenneth@resight.no>
2018-09-18 21:36:14 +0200

    #14 Create radio buttons to choose category

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import TextGetter from './TextGetter'
+import RadioGroup from './RadioGroup'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<TextGetter/>, document.getElementById('buson'));
+ReactDOM.render(<RadioGroup/>, document.getElementById('radioselector'));
 registerServiceWorker();

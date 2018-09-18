ca18817f853fc9168dc94d0911e2c080ba21bed6
Kenneth Wannebo <kenneth@resight.no>
2018-09-18 21:56:58 +0200

    #13 remove music specifics from App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AudioFile from './AudioFile';
-import song1 from './338986__drfx__music-box-melody-1.mp3';
-import song2 from './321718__rhodesmas__dark-magic-music-loop.wav';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {src: song1};
-    this.changeSource = this.changeAudioSource.bind(this);
-    this.child = React.createRef();
-  }
-
-  changeAudioSource() {
-    this.setState({src: song2});
-    this.child.current.reloadPlayer();
-  }
-
   render() {
     return (
       <div className="App">
@@ -25,11 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
-        <div>
-          <AudioFile src={this.state.src} ref={this.child} />
-          <button onClick={this.changeAudioSource}>Change song</button>
-        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }

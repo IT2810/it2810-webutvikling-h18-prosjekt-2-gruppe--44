69c2ff56cea6157f2502cff2a242b0979096d904
Ahsan <ahsan.azim@hotmail.com>
2018-09-20 07:21:04 +0200

    Revert Accidental push to master
    Commit: a13cc11
    I accidentally pushed to master therefore I am reverting the changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import PictureFetch from "./PictureFetch";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
+it('renders without crashing', () => {
+  const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.render(<PictureFetch />, div);
-
-  //ReactDOM.render(<App/>, document.querySelector("#main"));
   ReactDOM.unmountComponentAtNode(div);
 });
